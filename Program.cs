b4b4236f28b0ed9ac97721a09929e14ded8baa37
Praveen Lanka <praveenkumarlanka@outlook.com>
2023-07-14 15:05:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,390 @@
+﻿using System.Drawing;
+using System.Text;
+
+namespace ImLearningCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.BackgroundColor = ConsoleColor.Green;
+            //Console.WriteLine("Hello, World!");
+            //Console.ForegroundColor = ConsoleColor.Red;
+            //Console.WindowHeight = 20;
+            //Console.WriteLine(Console.CapsLock);
+            //Console.Clear();
+
+
+            // Console.WriteLine("Read Key");
+            //var userInput = Console.Read();
+            //var userInput1 = Console.Read();
+            //var userInput2 = Console.Read();
+            //Console.WriteLine(userInput);
+            //Console.WriteLine(userInput1);
+            //Console.WriteLine(userInput2);
+
+            //var vrKey = Console.ReadKey();
+            //Console.WriteLine();
+            //Console.WriteLine(vrKey.KeyChar);
+
+            //Console.WriteLine("My Name is Praveen Lanka");
+
+            //Console.WriteLine("Sunday");
+            //Console.WriteLine("Monday");
+            //Console.WriteLine("Tuesday");
+            //Console.WriteLine("Wednesday");
+            //Console.WriteLine("Thursday");
+            //Console.WriteLine("Friday");
+            //Console.WriteLine("Saturday");
+
+
+            #region Read
+            //Console.WriteLine("Enter First Friend Name:");
+            //var InputFirstName = Console.ReadLine();
+            //Console.ReadKey();
+
+
+            //Console.WriteLine("Enter Second Friend Name:");
+            //var InputSecondName = Console.ReadLine();
+            //Console.ReadKey();
+
+            //Console.WriteLine("Enter Third Friend Name:");
+            //var InputThirdName = Console.ReadLine();
+            //Console.ReadKey();
+
+            //Console.WriteLine("Enter Fourth Friend Name:");
+            //var InputFourthName = Console.ReadLine();
+            //Console.ReadKey();
+
+            //Console.WriteLine("Enter Fifth Friend Name:");
+            //var InputFiveName = Console.ReadLine();
+            //Console.ReadKey();
+
+            //Console.BackgroundColor = ConsoleColor.Green;
+            //Console.ForegroundColor = ConsoleColor.Blue;
+            //Console.WriteLine($"First Name is {InputFirstName}");
+
+            //Console.BackgroundColor = ConsoleColor.Red;
+            //Console.ForegroundColor = ConsoleColor.Black;
+            //Console.WriteLine($"Second Name is :{InputSecondName}");
+
+
+            //Console.BackgroundColor = ConsoleColor.Green;
+            //Console.ForegroundColor = ConsoleColor.Blue;
+            //Console.WriteLine($"Third Name is {InputThirdName}");
+
+            //Console.BackgroundColor = ConsoleColor.Green;
+            //Console.ForegroundColor = ConsoleColor.Blue;
+            //Console.WriteLine($"Foruth Name is :{InputFourthName}");
+
+            //Console.BackgroundColor = ConsoleColor.DarkYellow;
+            //Console.ForegroundColor = ConsoleColor.Blue;
+            //Console.WriteLine($"Five Name is {InputFiveName}");
+
+
+            //Console.ReadKey();
+
+            #endregion
+
+
+            #region Int
+
+            //   int a = 0;
+            //Console.WriteLine(a);
+
+
+            //int a, b, c = 10;
+            //b = c;
+            //Console.WriteLine(b);
+
+
+
+            //Console.WriteLine(default(int));
+            //Console.WriteLine(default(bool));
+
+            //int a = -110;
+            //int b = -20;
+            //Console.WriteLine("Is Positive : " + int.IsPositive(a));
+            //Console.WriteLine("Is Negative : " + int.IsNegative(b));
+
+            //int e = 30;
+            //int o = 82;
+            //Console.WriteLine("Is Even Number :" + int.IsEvenInteger(e));
+            //Console.WriteLine("Is Odd Number :" + int.IsOddInteger(o));
+
+
+            //Console.WriteLine("Integer Min Value :" + int.MinValue);  // -2147483648
+            //Console.WriteLine("Integer Max Value :" + int.MaxValue);  // 2147483647
+
+            //int a = 2147483647;
+            //Console.WriteLine(a + 1);
+            //Console.WriteLine(a + 2);
+
+
+
+
+
+            #endregion
+
+
+
+            #region Sizes
+
+            //Console.WriteLine("Size of SByte: " + sizeof(sbyte));
+            //Console.WriteLine("SByte Min Value:" + sbyte.MinValue);
+            //Console.WriteLine("SByte Max Value:" + sbyte.MaxValue + "\n");
+
+
+            //Console.WriteLine("Size of Byte: " + sizeof(byte));
+            //Console.WriteLine("Byte Min Value:" + byte.MinValue);
+            //Console.WriteLine("Byte Max Value:" + byte.MaxValue + "\n");
+
+
+            //Console.WriteLine("Size of Short: " + sizeof(short));
+            //Console.WriteLine("Short Min Value:" + short.MinValue);
+            //Console.WriteLine("Short Max Value:" + short.MaxValue + "\n");
+
+
+            //Console.WriteLine("Size of UShort: " + sizeof(ushort));
+            //Console.WriteLine("UShort Min Value:" + ushort.MinValue);
+            //Console.WriteLine("UShort Max Value:" + ushort.MaxValue + "\n");
+
+
+            //Console.WriteLine("Size of Int: " + sizeof(int));
+            //Console.WriteLine("Int Min Value:" + int.MinValue);
+            //Console.WriteLine("Int Max Value:" + int.MaxValue + "\n");
+
+            //Console.WriteLine("Size of UInt: " + sizeof(uint));
+            //Console.WriteLine("UInt Min Value:" + uint.MinValue);
+            //Console.WriteLine("UInt Max Value:" + uint.MaxValue + "\n");
+
+            //Console.WriteLine("Size of Long: " + sizeof(long));
+            //Console.WriteLine("Long Min Value:" + long.MinValue);
+            //Console.WriteLine("Long Max Value:" + long.MaxValue + "\n");
+
+            //Console.WriteLine("Size of ULong: " + sizeof(ulong));
+            //Console.WriteLine("ULong Min Value:" + ulong.MinValue);
+            //Console.WriteLine("ULong Max Value:" + ulong.MaxValue + "\n");
+
+
+
+
+            #endregion
+
+
+            #region Decimals
+
+            //Console.WriteLine("Size of Float:" + sizeof(float));
+            //Console.WriteLine("Min Value of Float: " + float.MinValue);
+            //Console.WriteLine("Max Value of Float :" + float.MaxValue + "\n");
+
+
+
+            //Console.WriteLine("Size of Double:" + sizeof(double));
+            //Console.WriteLine("Min Value of Double: " + double.MinValue);
+            //Console.WriteLine("Max Value of Double :" + double.MaxValue + "\n");
+
+
+            //Console.WriteLine("Size of Decimal:" + sizeof(decimal));
+            //Console.WriteLine("Min Value of Decimal: " + decimal.MinValue);
+            //Console.WriteLine("Max Value of Decimal :" + decimal.MaxValue + "\n");
+
+
+
+            //var floatvar = 1.0903e3M;
+            //Console.WriteLine(floatvar);
+
+
+            //var Decimalvar = 1.093333333333303e3M;
+            //Console.WriteLine(Decimalvar);
+
+
+            //var Doublevar = 1.093333333333303e3m;
+            //Console.WriteLine(Doublevar);
+
+
+
+
+
+            #endregion
+
+
+
+            #region chars
+
+            // Console.WriteLine("Enter Charecters :::");
+
+            // var char1 = 'A';
+            // Console.WriteLine("Charecter Value :: " + char1);
+
+            // var char2 = 'a';
+            // Console.WriteLine("Charecter Value :: " + char2);
+
+            //System.Char char3 = 'A';
+            // Console.WriteLine("Charecter 3 ::: " + char3);
+
+            // char char4 = '1';
+            // Console.WriteLine("Charecter 4 ::: " + char.IsLetterOrDigit(char4));
+
+
+
+            #endregion
+
+
+
+            #region Strings
+
+            Console.WriteLine("String Related:");
+
+
+            //string var2 = "This is Second String";
+
+            ////foreach (var x in var2)
+            ////{
+            ////    Console.WriteLine(x);
+
+            ////}
+
+            //// Console.WriteLine(var2[10]);
+
+
+            //char[] var3 = { 'A', 'B', 'D', 'F' };
+            //string newVar = new string(var3);
+            //Console.WriteLine(newVar);
+
+
+            // Console.WriteLine("This is Praveen\\n \\tIpad ");
+
+
+            //String Var1 = @"This is Praveen \\n \\tSuper";
+            //Console.WriteLine(Var1);
+
+
+            //string multiline = @"This is Praveen
+            //    Lanka
+            //    Kuma
+            //    Praveeen";
+
+            //Console.WriteLine(multiline);   
+
+
+            //string multiline = @"This is ""Praveen Lanka""";
+            //Console.WriteLine(multiline);
+
+
+            //string multiline = """
+            //    {"products":[{"id":1,"title":"iPhone 9","description":"An apple mobile which is nothing like apple","price":549,"discountPercentage":12.96,"rating":4.69,"stock":94,"brand":"Apple","category":"smartphones","thumbnail":"https://i.dummyjson.com/data/products/1/thumbnail.jpg","images":["https://i.dummyjson.com/data/products/1/1.jpg","https://i.dummyjson.com/data/products/1/2.jpg","https://i.dummyjson.com/data/products/1/3.jpg","https://i.dummyjson.com/data/products/1/4.jpg","https://i.dummyjson.com/data/products/1/thumbnail.jpg"]}
+
+            //    """;
+            //Console.WriteLine(multiline);
+
+
+
+            //string strTemp = "Praveen Kumar Lanka";
+
+            //var strFinal = strTemp.Length;
+            //Console.WriteLine("String Length: " + strFinal);
+
+            //Console.WriteLine("String Lower ::" + strTemp .ToLower());
+            //Console.WriteLine("String Upper ::" + strTemp.ToUpper());
+            //Console.WriteLine("String Trim Start ::" + strTemp.TrimStart());
+            //Console.WriteLine("String Trim End ::" + strTemp.TrimEnd());
+            //Console.WriteLine("String Replace ::" + strTemp.Replace('a', 'i'));
+            //Console.WriteLine("String Remove ::" + strTemp.Remove(6));
+
+            //Console.WriteLine("String Remove ::" + strTemp.Remove(0,3));
+
+            //var Final= strTemp.Split(' ');
+
+            //foreach (var i in Final) 
+            //{
+            //  Console.WriteLine($"{i}");
+            //}
+            #endregion
+
+
+            #region MutableImMutable
+
+            //string name = "Praveen";
+            // Console.WriteLine(name);   
+
+            //name += "Kumar";
+
+            //Console.WriteLine(name);
+
+            //for (int i=0; i <100;  i++) 
+            //{
+            //   name += " added" ; 
+
+            //}
+
+            //Console.WriteLine(name);
+
+            #endregion
+
+
+
+            #region StringBuilder
+            //var a = new StringBuilder(10,15);
+
+            //a.Append("Praven");
+
+
+
+            ////for(int i=0;i<10;i++) 
+            ////{ 
+            ////    Console.WriteLine(a.ToString());
+            ////}
+
+            //a.AppendFormat("Nithish {0} {1}", 12, 34);
+
+            //Console.WriteLine(a.ToString());
+
+
+            #endregion
+
+
+            #region StringFormatConcats
+
+            //Console.WriteLine("Hello" + " "   + " World!");
+
+            //string firstname = "Praveen";
+            //string lastName = "Lanka";
+
+            //string fullname = firstname + " " + lastName;   
+            //Console.WriteLine("FullName :" + fullname);
+
+
+            //string author = "Mahesh Chand";
+
+            //string books = "c# programming";
+
+            //string booKAuthor = $"{author} , {books} ,  {DateTime.Now.Year}";
+
+            //Console.WriteLine("Book Author: " + booKAuthor);
+
+
+            string firstname = "Praveen";
+            string lastName = "Lanka";
+            DateTime dateTime = DateTime.Now;
+            int age = 20;
+
+            //Console.WriteLine(string.Format("{0}  {1} {2} age is {3}", firstname, lastName , dateTime,age));
+
+            var name = (fN:"Praveen", lN: "Lanka");
+            Console.WriteLine(name.fN.ToString());
+            Console.WriteLine(name.lN.ToString());
+
+
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(string.Format("{0:MM/dd/yyyy hh:mm tt}", dt));
+
+
+            string FullName = $"{firstname} - {lastName}";
+            Console.WriteLine(FullName);
+
+            #endregion
+
+        }
+    }
+}
