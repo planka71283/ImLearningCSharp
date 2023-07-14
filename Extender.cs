254f65d80d3ebf583ee7e22d5036b7f13ab8933d
Praveen Lanka <praveenkumarlanka@outlook.com>
2023-07-14 15:36:35 +0530

    Initial Commit

---
+++ Extender.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImLearningCSharp
+{
+    internal class Extender
+    {
+
+        public void FirstMethod()
+        {
+            return;
+        }
+
+
+            public void SecondMethod()
+            {
+                return;
+            }
+
+            public void ThirdMethod()
+            {
+                return;
+
+            }
+        }
+}
